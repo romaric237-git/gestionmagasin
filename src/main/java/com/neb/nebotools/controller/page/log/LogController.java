c8144ddd7c171d0b0ed2ed9dde86680b3e72bc19
Njotsa Ella Brondon <rocks@rocks.cm>
2024-04-19 18:33:07 +0100

    fix error

--- src/main/java/com/neb/nebotools/controller/page/log/LogController.java
+++ src/main/java/com/neb/nebotools/controller/page/log/LogController.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class LogController extends ControllerListAbstract<Log> {
     @Override
     protected void setDao() throws SQLException {
-dao = DaoFactory.ge
+//dao = DaoFactory.ge
     }
 
     @Override
